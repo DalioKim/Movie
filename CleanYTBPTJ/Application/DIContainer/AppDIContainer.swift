72cd62d4d981e2897916f51ffc52ff29ea033ddf
DalioKim <ejxlabvlzhxm@naver.com>
2022-02-15 18:14:35 +0900

    FIX: 옵셔널 강제 해제 수정 작업 진행중 AppDIContainer 파일의 baseURL을 강제해제에서 if let 바인딩 방식으로 수정함에 따라, 의존성 파일들 변경필요

--- CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
+++ CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
@@ -7,24 +7,23 @@ final class AppDIContainer {
     lazy var appConfiguration = AppConfiguration()
     
     // MARK: - Network
-    lazy var apiDataTransferService: DataTransferService = {
-        //유튜브 할당량 초과
-//        let config = ApiDataNetworkConfig(baseURL: URL(string: appConfiguration.apiBaseURL)!,
-//                                          queryParameters: ["key": appConfiguration.apiKey])
-        
-        //네이버 오픈 api
-        let config = ApiDataNetworkConfig(baseURL: URL(string: appConfiguration.apiBaseURL)!,
-                                          headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
-        
-        let apiDataNetwork = DefaultNetworkService(config: config)
-        return DefaultDataTransferService(with: apiDataNetwork)
+    lazy var apiDataTransferService: DataTransferService? = {
+        if let baseURL = URL(string: appConfiguration.apiBaseURL) {
+            let config = ApiDataNetworkConfig(baseURL: baseURL,
+                                              headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
+            
+            let apiDataNetwork = DefaultNetworkService(config: config)
+            return DefaultDataTransferService(with: apiDataNetwork)
+        } else {
+            return nil
+        }
     }()
- 
+    
     
     // MARK: - DIContainers of scenes
     func makeMovieSceneDIContainer() -> MovieSceneDIContainer {
         printIfDebug("AppDIContainer makeThumbnailsSceneDIContainer")
-        let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService)
+        let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService!)
         return MovieSceneDIContainer(dependencies: dependencies)
     }
 }
