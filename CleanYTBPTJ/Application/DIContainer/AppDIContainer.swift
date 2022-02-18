74d341c948422ca5b7fb6776f208206245ed5df7
DalioKim <ejxlabvlzhxm@naver.com>
2022-02-18 17:34:06 +0900

    REFACTOR : If let 바인딩을 Guard let 바인딩으로 변경

--- CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
+++ CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
@@ -9,19 +9,15 @@ final class AppDIContainer {
     // MARK: - Network
     
     lazy var apiDataTransferService: DataTransferService = {
-        if let baseURL = URL(string: appConfiguration.apiBaseURL) {
-            let config = ApiDataNetworkConfig(baseURL: baseURL,
-                                              headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
-            
-            let apiDataNetwork = DefaultNetworkService(config: config)
-            return DefaultDataTransferService(with: apiDataNetwork)
-        } else {
-            fatalError("API주소 URL변경 에러")
-        }
+        guard let baseURL = URL(string: appConfiguration.apiBaseURL) else { fatalError("API주소 URL변경 에러") }
+        let config = ApiDataNetworkConfig(baseURL: baseURL,
+                                          headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
+        let apiDataNetwork = DefaultNetworkService(config: config)
+        return DefaultDataTransferService(with: apiDataNetwork)
     }()
     
-    
     // MARK: - DIContainers of scenes
+    
     func makeMovieSceneDIContainer() -> MovieSceneDIContainer {
         printIfDebug("AppDIContainer makeThumbnailsSceneDIContainer")
         let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService)
