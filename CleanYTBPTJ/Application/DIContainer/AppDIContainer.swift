0ceb6dfc1a57c79f9791ed59838984e5b6a9fa4e
DalioKim <ejxlabvlzhxm@naver.com>
2022-02-18 14:48:25 +0900

    FIX :  baseURL 명시적 옵셔널 바인딩으로 변경

--- CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
+++ CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
@@ -7,7 +7,8 @@ final class AppDIContainer {
     lazy var appConfiguration = AppConfiguration()
     
     // MARK: - Network
-    lazy var apiDataTransferService: DataTransferService? = {
+    
+    lazy var apiDataTransferService: DataTransferService = {
         if let baseURL = URL(string: appConfiguration.apiBaseURL) {
             let config = ApiDataNetworkConfig(baseURL: baseURL,
                                               headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
@@ -15,7 +16,7 @@ final class AppDIContainer {
             let apiDataNetwork = DefaultNetworkService(config: config)
             return DefaultDataTransferService(with: apiDataNetwork)
         } else {
-            return nil
+            fatalError("API주소 URL변경 에러")
         }
     }()
     
@@ -23,7 +24,7 @@ final class AppDIContainer {
     // MARK: - DIContainers of scenes
     func makeMovieSceneDIContainer() -> MovieSceneDIContainer {
         printIfDebug("AppDIContainer makeThumbnailsSceneDIContainer")
-        let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService!)
+        let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService)
         return MovieSceneDIContainer(dependencies: dependencies)
     }
 }
