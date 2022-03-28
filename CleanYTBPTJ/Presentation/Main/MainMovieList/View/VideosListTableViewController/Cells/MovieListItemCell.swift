05bc3420a44a91416b7c50f355001f357e76cf73
DalioKim <ejxlabvlzhxm@naver.com>
2022-03-28 15:17:07 +0900

    REFACTOR: 1. enum Image의 네이밍 Thumbnail로 변경 2. enum Title의 breakMode 네이밍 lineBreakMode로 변경

--- CleanYTBPTJ/Presentation/Main/MainMovieList/View/VideosListTableViewController/Cells/MovieListItemCell.swift
+++ CleanYTBPTJ/Presentation/Main/MainMovieList/View/VideosListTableViewController/Cells/MovieListItemCell.swift
@@ -9,7 +9,7 @@ class MovieListItemCell: UICollectionViewCell {
         static let horizontalPadding: CGFloat = 20
         static let verticalPadding: CGFloat = 10
         static let spacing: CGFloat = 10
-        enum Image {
+        enum Thumbnail {
             static let width: CGFloat = 40
             static let height: CGFloat = 60
         }
@@ -18,7 +18,7 @@ class MovieListItemCell: UICollectionViewCell {
         enum Title {
             static let font = UIFont.systemFont(ofSize: 16)
             static let lines = 10
-            static let breakMode: NSLineBreakMode = .byTruncatingTail
+            static let lineBreakMode: NSLineBreakMode = .byTruncatingTail
         }
     }
     
@@ -31,8 +31,8 @@ class MovieListItemCell: UICollectionViewCell {
         stackView.alignment = .center
         
         thumbnailImageView.snp.makeConstraints {
-            $0.width.equalTo(Size.Image.width)
-            $0.height.equalTo(Size.Image.height)
+            $0.width.equalTo(Size.Thumbnail.width)
+            $0.height.equalTo(Size.Thumbnail.height)
         }
         
         return stackView
@@ -42,7 +42,7 @@ class MovieListItemCell: UICollectionViewCell {
         titleLabel.numberOfLines = Style.Title.lines
         titleLabel.textAlignment = .left
         titleLabel.font = Style.Title.font
-        titleLabel.lineBreakMode = Style.Title.breakMode
+        titleLabel.lineBreakMode = Style.Title.lineBreakMode
         return titleLabel
     }()
     private let thumbnailImageView: UIImageView = {
@@ -82,7 +82,7 @@ class MovieListItemCell: UICollectionViewCell {
         guard let model = model else { return }
         self.viewModel = model
         titleLabel.attributedText = model.title.applyTag()
-        updateThumbnailImage(width: Int(Size.Image.width))
+        updateThumbnailImage(width: Int(Size.Thumbnail.width))
     }
     
     private func updateThumbnailImage(width: Int) {
@@ -93,9 +93,9 @@ class MovieListItemCell: UICollectionViewCell {
     }
     
     static func size(width: CGFloat, model: MovieListItemCellModel) -> CGSize {
-        let titleWidth = width - Size.Image.width - Size.spacing - (Size.horizontalPadding * 2)
-        let titleHeight = CalcText.height(attributedText: model.title.applyTag(), lineBreakMode: Style.Title.breakMode, numberOfLines: Style.Title.lines, width: titleWidth)
-        let itemHeight = max(Size.Image.height, titleHeight) + (Size.verticalPadding * 2)
+        let titleWidth = width - Size.Thumbnail.width - Size.spacing - (Size.horizontalPadding * 2)
+        let titleHeight = CalcText.height(attributedText: model.title.applyTag(), lineBreakMode: Style.Title.lineBreakMode, numberOfLines: Style.Title.lines, width: titleWidth)
+        let itemHeight = max(Size.Thumbnail.height, titleHeight) + (Size.verticalPadding * 2)
         return CGSize(width: width, height: itemHeight)
     }
 }
