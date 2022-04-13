725b6d2eb55b19386869ce437892d61a1e05944a
“DalioKim” <ejxlabvlzhxm@naver.com>
2022-04-13 22:10:54 +0900

    FIX: 삭제 파일 복구 AppConfiguration, AppDIContainer

--- CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
+++ CleanYTBPTJ/Application/DIContainer/AppDIContainer.swift
@@ -5,21 +5,9 @@ final class AppDIContainer {
     
     lazy var appConfiguration = AppConfiguration()
     
-    // MARK: - Network
-    
-    lazy var apiDataTransferService: DataTransferService = {
-        guard let baseURL = URL(string: appConfiguration.apiBaseURL) else { fatalError("API주소 URL변경 에러") }
-        let config = ApiDataNetworkConfig(baseURL: baseURL,
-                                          headers: ["X-Naver-Client-Id": appConfiguration.id,"X-Naver-Client-Secret": appConfiguration.apiKey])
-        let apiDataNetwork = DefaultNetworkService(config: config)
-        return DefaultDataTransferService(with: apiDataNetwork)
-    }()
-    
     // MARK: - DIContainers of scenes
     
     func makeMovieSceneDIContainer() -> MovieSceneDIContainer {
-        printIfDebug("AppDIContainer makeThumbnailsSceneDIContainer")
-        let dependencies = MovieSceneDIContainer.Dependencies(apiDataTransferService: apiDataTransferService)
-        return MovieSceneDIContainer(dependencies: dependencies)
+        return MovieSceneDIContainer()
     }
 }
